972f2eb3a18640b1d0115ce3075df6ce4241bc23
coderjun <coderjun@foxmail.com>
2021-07-07 19:30:17 +0800

    1.0.3

--- bundle.js
+++ bundle.js
@@ -3561,11 +3561,16 @@ const wjEthInputAddress = document.querySelector('.wjEthInputAddress');
 const wjCurrentAddress = document.querySelector('.wjCurrentAddress');
 //当前操作链
 const wjCurrentChain = document.querySelector('.wjCurrentChain');
+//获取结果列表
+const wjTxResultList = document.querySelector('.wjTxResultList');
 
 const ether = new BN('de0b6b3a7640000', 16);
 const gasPrice = new BN('4a817c800',16)
+const gasAmount = '0x5208';
+
 
 let accounts = [];
+let txResults = [];
 // let addresses = [];
 // 预加载方法
 getAccount();
@@ -3598,7 +3603,7 @@ async function checkXdaiNet(){
     var netId = await ethereum.request({method:'net_version'});
     console.log(netId)
     if (netId == '100'){
-
+        wjCurrentChain.innerHTML = '100';
     }
     else{
        alert('请切换到Xdai网络');
@@ -3626,13 +3631,14 @@ function addXdaiNew(){
 
 //发送交易点击监听事件
 wjEthSendButton.addEventListener('click', () => {
-    //获取计算数量
-    var count = new BN(wjEthValueInput.value,10);
     //使用一以太为单位计算总共花费
-    var total =  count.mul(ether);
+    var total =  ethConut(wjEthValueInput.value);
     var addresses = wjEthInputAddress.value.split('\n');
-    console.log(addresses);
 
+    //清除结果列表
+    txResults = [];
+    
+    //打包交易
     for(var i =0 ; i < addresses.length; i++ ){
         transaction(accounts[0],addresses[i],total)
     }
@@ -3660,12 +3666,26 @@ async  function transaction (fromCount,toCount,value) {
                     to: toCount,
                     value: value.toString(16),
                     gasPrice: gasPrice.toString(16),
-                    gas: '0x5208',
+                    gas: gasAmount,
                 },
             ],
         })
-        .then((txHash) => console.log(txHash))   //交易hash
-        .catch((error) => console.error);        //报错信息
+        .then((txHash) => {
+            //刷新列表
+            txResults.push(txHash);
+            var tr = wjTxResultList.insertRow(txResults.length)
+            var td = tr.insertCell(0);
+            td.innerHTML = txHash;
+            td.onclick = function(){
+                console.log(txHash);
+                //跳转新的页面 https://blockscout.com/poa/xdai/tx/0x10380e58004e7319a601bd7113bc41b332bba64e79652fcb4958ee208e2d5cf8
+                var baseUrl = "https://blockscout.com/poa/xdai/tx/"+txHash;
+
+                window.open(baseUrl);
+            };        
+            console.log(td)   
+        })   //交易hash
+        .catch((error) => console.log(error));        //报错信息
 }
 
 //监听事件
@@ -3680,15 +3700,45 @@ ethereum.on('accountsChanged', function (accounts) {
 ethereum.on('chainChanged', function (chainString) {
     // Time to reload your interface with accounts[0]!
     var str = chainString.substr(2);
-    var chainId = new BN(str,16)
-    wjCurrentChain.innerHTML = chainId.toString(10);
+    wjCurrentChain.innerHTML = formartChain(str);
     // console.log(chainId.toString(10));
 });
 
 ethereum.on('connect',function (accounts) {
-    wjCurrentChain.innerHTML = accounts['chainId'];
+    wjCurrentChain.innerHTML = formartChain(accounts['chainId']);
     console.log("connect"+accounts);
 });
+
+//tools
+function formartChain(str) {
+    var chainId = new BN(str,16);
+    return chainId.toString(10);
+}
+
+
+function ethConut(str){
+    var location = String(str).indexOf('.') + 1;
+    if(location > 0) {
+        //是小数 
+        var length = String(str).length - location;
+        //进位去除小数
+        var ethValue = parseFloat(String(str))*parseInt(Math.pow(10,length).toString());
+        var totoalBn = new BN(ethValue.toString(10),10)
+        totoalBn.imul(ether);
+        //除以之前的进位
+        totoalBn.idivn(Math.pow(10,length));
+        return totoalBn;
+    }
+    else {
+        //不是小数
+        var totoalBn = new BN(String(str),10);
+        totoalBn.imul(ether);
+        return totoalBn;
+    }
+  
+}
+
+
 },{"bn.js":1}],3:[function(require,module,exports){
 
 },{}]},{},[2]);
